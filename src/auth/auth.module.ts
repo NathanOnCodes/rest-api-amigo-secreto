78c4ae15ec08a4c106f26cf55ec18b670026673f
NathanOnCodes <contatonathancf@gmail.com>
2025-03-01 21:25:15 -0300

    fix: adjust imports and providers

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,29 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants/constants';
+import { HashService } from './hash.service';
+import { User } from 'src/users/entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule,
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forFeature([User]), 
+    forwardRef(() => UsersModule),
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
       signOptions: { expiresIn: '300000s' },
     })
   ],
-  providers: [AuthService],
-  controllers: [AuthController]
+  providers: [AuthService, HashService],
+  controllers: [AuthController],
+  exports: [AuthService, HashService]
 })
 export class AuthModule {}
+
+
