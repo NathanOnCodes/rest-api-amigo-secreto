e5d30eb419e39ae3d2dcf8b97811250378b85824
NathanOnCodes <contatonathancf@gmail.com>
2025-03-12 14:07:46 -0300

    feat: add token-paylod

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,3 +1,10 @@
-export class AuthDto {
-    email: string;
-  }
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class LoginDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
